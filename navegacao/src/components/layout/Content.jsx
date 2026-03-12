ce3d42c8ae707574fdfa1ace4992d180f459a280
João Pedro Moro Bolognini <joaopedromorob@gmail.com>
2026-03-12 15:40:17 -0300

    Aplicando estilo no projeto

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './Content.css';
 
 const Content = props => (
-    <main className='Content'>Componente Content</main>
+    <main className='Content'>
+        <h1>Componente Content</h1>
+    </main>
 );
 
 export default Content;
--- navegacao/src/components/layout/Menu.jsx
+++ navegacao/src/components/layout/Menu.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './Menu.css';
 
 const Menu = props => (
-    <aside className='Menu'>Componente Menu</aside>
+    <aside className='Menu'>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Início</a>
+                </li>
+                <li>
+                    <a href="/">Sobre</a>
+                </li>
+            </ul>
+        </nav>
+    </aside>
 );
 
 export default Menu;
