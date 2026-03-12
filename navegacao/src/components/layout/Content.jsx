0f8c5dff923e8f6a5e0a47e7cab4d2752dcd5b38
João Pedro Moro Bolognini <joaopedromorob@gmail.com>
2026-03-12 15:50:27 -0300

    Setando as rotas início e sobre

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Content.css';
+import { Routes, Route } from "react-router-dom"
+
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
 
 const Content = props => (
     <main className='Content'>
-        <h1>Componente Content</h1>
+        <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
     </main>
 );
 
