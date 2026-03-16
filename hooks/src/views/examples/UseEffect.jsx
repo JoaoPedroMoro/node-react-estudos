09fc66c67dc2430e5d5e0698390fed7dffdf2e80
João Pedro Moro Bolognini <joaopedromorob@gmail.com>
2026-03-16 14:05:03 -0300

    Adicionando mais um exercício no UseEffect

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -13,10 +13,16 @@ const UseEffect = (props) => {
 
     const [num, setNum] = useState(1)
     const [fatorial, setFatorial] = useState(1)
+    const [num2, setNum2] = useState(0)
+    const [status, setStatus] = useState("Par")
 
     useEffect(function () {
         setFatorial(calcFatorial(num))
     }, [num])
+
+    useEffect(function () {
+        setStatus(num2 % 2 === 0 ? "Par" : "Ímpar")
+    }, [num2])
 
     useEffect(function () {
         if (fatorial > 1000) {
@@ -40,14 +46,20 @@ const UseEffect = (props) => {
                     <span className="text">Fatorial: </span>
                     <span className="text red">{fatorial === -1 ? "Não existe!" : fatorial}</span>
                 </div>
-                <input type="number" className="input" 
-                value={num} 
-                onChange={e => setNum(Number(e.target.value))}/>
+                <input type="number" className="input"
+                    value={num}
+                    onChange={e => setNum(Number(e.target.value))} />
             </div>
 
             <SectionTitle title="Exercício #02" />
             <div className="center">
-                
+                <div>
+                    <span className="text">Status: </span>
+                    <span className="text red">{status}</span>
+                </div>
+                <input type="number" className="input"
+                    value={num2}
+                    onChange={e => setNum2(Number(e.target.value))} />
             </div>
 
         </div>
