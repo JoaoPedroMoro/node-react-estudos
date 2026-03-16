8f89d654aeb19c6717a7d71056720d23a84fb95f
João Pedro Moro Bolognini <joaopedromorob@gmail.com>
2026-03-16 09:38:22 -0300

    Arrumando a versão do router e implementando useState

--- hooks/src/views/examples/UseState.jsx
+++ hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseState = (props) => {
+
+    const [contador, setContador] = useState(0)
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text">{contador}</span>
+                <div>
+                    <button className="btn" onClick={() => setContador(contador - 1)}>
+                        -1
+                    </button>
+                    <button className="btn" onClick={() => setContador(contador + 1)}>
+                        +1
+                    </button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
         </div>
     )
 }
